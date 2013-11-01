5a57344ba21e0fe59da240b9549717ff297c30ab
buckmarble <forrestmarble@gmail.com>
2013-10-31 21:30:03 -0400

    Revert "USB force fast charging"
    This reverts commit 9837a742237083e05c36c24bd55e53b5313f0bfe.

--- build_m7_kernel.sh
+++ build_m7_kernel.sh
@@ -7,16 +7,6 @@
 # Date to add to zip
   today=$(date +"%m_%d_%Y")
 
-# Clean old builds
-   echo "Clean"
-     rm -rf $k/out
-#     make clean
-
-# Setup the build
- cd $k/arch/arm/configs/BBKconfigs
-    for c in *
-      do
-        cd $k
 # Setup output directory
        mkdir -p "out/$c"
           cp -R "$t/system" out/$c
@@ -32,33 +22,28 @@ export ARCH=arm
 export SUBARCH=arm
 
 # make mrproper
-#make CROSS_COMPILE=$TOOLCHAIN -j`grep 'processor' /proc/cpuinfo | wc -l` mrproper
- 
+make CROSS_COMPILE=$TOOLCHAIN -j`grep 'processor' /proc/cpuinfo | wc -l` mrproper
+
 # remove backup files
 find ./ -name '*~' | xargs rm
-# rm compile.log
+rm compile.log
 
 # make kernel
 make elite_m7_defconfig
-make -j`grep 'processor' /proc/cpuinfo | wc -l` CROSS_COMPILE=$TOOLCHAIN #>> compile.log 2>&1 || exit -1
+make -j`grep 'processor' /proc/cpuinfo | wc -l` CROSS_COMPILE=$TOOLCHAIN >> compile.log 2>&1 || exit -1
 
-# Grab modules & zImage
-   echo ""
-   echo "<<>><<>>  Collecting modules and zimage <<>><<>>"
-   echo ""
-   cp $k/arch/arm/boot/zImage out/$c/kernel/kernel
-   for mo in $(find . -name "*.ko"); do
-		cp "${mo}" $m
-   done
+# copy modules
+find -name '*.ko' -exec cp -av {} ../m7-vzw/system/lib/modules/ \;
 
-# Build Zip
- clear
-   echo "Creating $z.zip"
-     cd $k/out/$c/
-       7z a "$z.zip"
-         mv $z.zip $k/out/$z.zip
-# cp $k/out/$z.zip $db/$z.zip
-#           rm -rf $k/out/$c
-# Line below for debugging purposes,  uncomment to stop script after each config is run
-#read this
-      done
+# copy kernel image
+cp arch/arm/boot/zImage /home/forrest/kernel/android_kernel_htc_m7/out/kernel/kernel
+
+# strip modules
+${TOOLCHAIN}strip --strip-unneeded /home/forrest/kernel/android_kernel_htc_m7/out/system/lib/modules/*ko
+
+# create cwm zip
+cd 
+find ./ -name '*~' | xargs rm
+rm *.zip
+TIMESTAMP=Elite-`date +%Y%m%d-%T`
+zip -r m7-$today.zip *
