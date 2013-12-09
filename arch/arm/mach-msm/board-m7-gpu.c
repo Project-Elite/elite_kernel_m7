9b6497033c4d3152ba7a954c113982edb2edf141
buckmarble <forrestmarble@gmail.com>
2013-12-09 13:49:48 -0500

    Revert "update: gpu/cpu"
    This reverts commit 19276c01e6a130d173d042c7015f32e495ce85e5.

--- arch/arm/mach-msm/board-m7-gpu.c
+++ arch/arm/mach-msm/board-m7-gpu.c
@@ -232,11 +232,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 0,
 		},
 		{
-			.gpu_freq = 400000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
-		{
 			.gpu_freq = 320000000,
 			.bus_freq = 3,
 			.io_fraction = 33,
@@ -257,7 +252,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = 6,
+	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.strtstp_sleepwake = true,
